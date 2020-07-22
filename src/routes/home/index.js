7bab4e8781be7e424a4a341d174aa09c4a0fa95d
d11r <itsdragos@gmail.com>
2020-07-22 18:20:04 +0200

    fix

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -546,16 +546,16 @@ const Home = () => {
         <i class="fa fa-whatsapp whatsapp-float"></i>
       </a>
 
-      <script src="../../style/js/jquery.min.js"></script>
-      <script src="../../style/js/modernizr-2.6.2.min.js"></script>
-      <script src="../../style/js/jquery.easing.1.3.js"></script>
-      <script src="../../style/js/bootstrap.min.js"></script>
-      <script src="../../style/js/jquery.waypoints.min.js"></script>
-      <script src="../../style/js/jquery.flexslider-min.js"></script>
-      <script src="../../style/js/sticky-kit.min.js"></script>
-      <script src="../../style/js/jquery.magnific-popup.min.js"></script>
-      <script src="../../style/js/owl.carousel.min.js"></script>
-      <script src="../../style/js/main.js"></script>
+      <script src="../../js/jquery.min.js"></script>
+      <script src="../../js/modernizr-2.6.2.min.js"></script>
+      <script src="../../js/jquery.easing.1.3.js"></script>
+      <script src="../../js/bootstrap.min.js"></script>
+      <script src="../../js/jquery.waypoints.min.js"></script>
+      <script src="../../js/jquery.flexslider-min.js"></script>
+      <script src="../../js/sticky-kit.min.js"></script>
+      <script src="../../js/jquery.magnific-popup.min.js"></script>
+      <script src="../../js/owl.carousel.min.js"></script>
+      <script src="../../js/main.js"></script>
     </div>
   );
 };
